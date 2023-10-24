4eee3520ebd6aa137c819c5cc5e7343bb5661127
BuildTools <unconfigured@null.spigotmc.org>
2023-10-24 17:14:12 +0200

    code refactor + other minor changes

---
+++ functions.py
@@ -0,0 +1,84 @@
+import os
+import yaml
+import matplotlib.pyplot as plt
+import numpy as np
+
+def energy_request(data):
+    for i in data["hours"]:
+        data["energy_delta"] = data["energy_pv"][i]- data["load_profile"][i]
+
+
+        if(data["energy_delta"] >= 0):                   #produco più di quel che consumo
+            if (i == 0):
+                if(data["initial_battery_level"] + data["energy_delta"] >= data["maximum_battery_level"]):      #batteria piena ,vendo alla rete(in futuro accumulo sopra soglia)
+                    data["energy_grid"][i] = data["initial_battery_level"] + data["energy_delta"] - data["maximum_battery_level"]
+                    data["battery_levels"][i] = data["maximum_battery_level"]
+
+                if(data["initial_battery_level"] + data["energy_delta"] < data["maximum_battery_level"]):        #batteria scarica ,la ricarico
+                    data["battery_levels"][i] = data["initial_battery_level"] + data["energy_delta"]
+            else:
+                if(data["battery_levels"][i-1] + data["energy_delta"] >= data["maximum_battery_level"]):      #batteria piena ,vendo alla rete(in futuro accumulo sopra soglia)
+                    data["energy_grid"][i] = data["battery_levels"][i-1] + data["energy_delta"] - data["maximum_battery_level"]
+                    data["battery_levels"][i] = data["maximum_battery_level"]
+
+                if(data["battery_levels"][i-1] + data["energy_delta"] < data["maximum_battery_level"]):        #batteria scarica ,la ricarico
+                    data["battery_levels"][i] = data["battery_levels"][i-1] + data["energy_delta"]
+
+        if(data["energy_delta"] < 0):                     #consumo più di quello che produco
+            if(i == 0):
+                if(data["initial_battery_level"] > data["minimum_battery_level"] + abs(data["energy_delta"]) ):       #la batteria ha sufficiente energia per alimentare il carico
+                    data["battery_levels"][i] = data["initial_battery_level"] - abs(data["energy_delta"])
+                    
+                if(data["initial_battery_level"] < data["minimum_battery_level"] + abs(data["energy_delta"])):                #la batteria non ha sufficiente energia(totale), prendo energia dalla rete
+                    data["energy_grid"][i] = - ( abs(data["energy_delta"]) - ( data["initial_battery_level"] - data["minimum_battery_level"] ) )
+                    data["battery_levels"][i] = data["minimum_battery_level"]
+            else:
+                if(data["battery_levels"][i-1] > data["minimum_battery_level"] + abs(data["energy_delta"]) ):       #la batteria ha sufficiente energia per alimentare il carico
+                    data["battery_levels"][i] = data["battery_levels"][i-1] - abs(data["energy_delta"])
+                    
+                if(data["battery_levels"][i-1] < data["minimum_battery_level"] + abs(data["energy_delta"])):                #la batteria non ha sufficiente energia(totale), prendo energia dalla rete
+                    data["energy_grid"][i] = - ( abs(data["energy_delta"]) - ( data["battery_levels"][i-1] - data["minimum_battery_level"] ) )
+                    data["battery_levels"][i] = data["minimum_battery_level"]
+
+    return data
+
+
+def setup(disablePV=0, disableBattery=0):
+
+    with open("conf.yaml") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    
+
+    data["energy_pv"] = [0,0,0,0,0,0,     0.5,0.75,1,1.6,2.25,3,      2.25,1.6,1,0.75,0.5,0,   0,0,0,0,0,0]
+    data["load_profile"] = [1,1,1,1,1,1.75,  2.5,1.75,1,1,1,1,    1,1,1,1,1,1,      1.75,2.5,1.75,1.6,1.5,1]
+    data["energy_delta"] = 0
+    data["battery_levels"] = []
+    data["hours"] = []
+    data["energy_grid"] = []
+
+    if disablePV:
+        data["energy_pv"] = []
+    if disableBattery:
+        data["maximum_battery_level"] = 0
+        data["minimum_battery_level"] = 0
+        data["initial_battery_level"] = 0
+
+    for i in range(0,24):
+        if disablePV:
+            data["energy_pv"].append(0)
+        data["hours"].append(i)
+        data["energy_grid"].append(0)
+        data["battery_levels"].append(data["initial_battery_level"])
+
+    
+    return data
+
+
+def plot_graph(x, y, xlabel, ylabel, title, color):
+    plt.figure(title)
+    plt.plot(x, y, label = title, color=color)
+    plt.title(title)
+    plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
--- main.py
+++ main.py
@@ -1,61 +1,15 @@
-import matplotlib.pyplot as plt
-def energy_request(profilo_di_carico,generazione_pannello,ora,E_b,E_max,E_min,E_grid):
-    for i in ora:
-        delta_Eb= generazione_pannello[i]- profilo_di_carico[i]
+from functions import *
 
-        if(delta_Eb>= 0):                   #produco più di quel che consumo
-        
-            if(E_b[i]+delta_Eb >= E_max):      #batteria piena ,vendo alla rete(in futuro accumulo sopra soglia)
-                E_grid[i]=E_b[i]+delta_Eb-E_max
-                E_b[i]=E_max
+data = setup(disablePV=True)
 
-            if(E_b[i]+delta_Eb < E_max):        #batteria scarica ,la ricarico
-                E_b[i]=E_b[i]+delta_Eb
+energy_request(data)
 
-        if(delta_Eb <0):                     #consumo più di quello che produco
-            if(E_b[i] > E_min+delta_Eb):       #batteria piena ,vendo alla rete(in futuro accumulo sopra soglia)
-                E_grid[i]=E_b[i]+delta_Eb-E_max
-                E_b[i]=E_max
-
-            if(E_b+delta_Eb < E_max):        #batteria scarica ,la ricarico
-                E_b[i]=E_b[i]+delta_Eb
-
-print("ciao aadasdasd")
-#espressi in kw/h
-profilo_di_carico= []
-generazione_pannello=[]
-ora=[]
-E_b=[]
-E_grid=[]
-battery_level=[]
-
-#popolamento 
-E_max=5
-E_min=1
-profilo_di_carico= [1,1,1,1,1,1.75,  2.5,1.75,1,1,1,1,    1,1,1,1,1,1,      1.75,2.5,1.75,1.6,1.5,1]
-generazione_pannello=[0,0,0,0,0,0,     0.5,0.75,1,1.6,2.25,3,      2.25,1.6,1,0.75,0.5,0,   0,0,0,0,0,0]
-
-for i in range(0,24):
-    ora.append(i)
-    E_grid.append(0)
-    E_b.append(E_min)
-
-plt.plot(ora,profilo_di_carico, label='Carico di casa', color='blue', linestyle='--')
-plt.xlabel('time')
-plt.ylabel('Kw/h')
-plt.title('Profilo di carico')
-plt.legend()
+plot_graph(data["hours"], data["load_profile"], "Time", "kW/h", "Load profile", "red")
+plot_graph(data["hours"], data["battery_levels"], "Time", "kW/h", "Battery Level", "green")
+plot_graph(data["hours"], data["energy_grid"], "Time", "kW/h", "Energy from/to Grid", "blue")
+plot_graph(data["hours"], data["energy_pv"], "Time", "kW/h", "Energy from PV", "yellow")
+print(data["load_profile"])
+print(data["battery_levels"])
+print(data["energy_grid"])
+print(data["energy_pv"])
 plt.show()
-
-
-plt.plot(ora,generazione_pannello, label='Generazione pannello', color='blue', linestyle='--')
-plt.xlabel('time')
-plt.ylabel('Kw/h')
-plt.title('Generazione pannello')
-plt.legend()
-plt.show()
-energy_request(profilo_di_carico,generazione_pannello,ora,E_min,E_max,E_min,E_grid)
-
-
-
-
