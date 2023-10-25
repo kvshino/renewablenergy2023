a011ed5a1483eb4cb842d284669dc11088cee9ec
valeriobattipaglia <v.battipaglia12@studenti.unisa.it>
2023-10-25 12:56:24 +0200

    output fix

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ data = setup()
 energy_request(data)
 
 dataframe=pd.DataFrame(data)
+print("prova")
 
 
-plot_graph(data, "load_profile", "kW/h", "Load profile", "#B55E60")
-plot_graph(data, "battery_levels", "kW/h", "Battery Level", "#609E6E")
-plot_graph(data, "energy_grid", "kW/h", "Energy from/to Grid", "#5975A5")
-plot_graph(data, "energy_pv", "kW/h", "Energy from PV", "#CC8962")
-#plot_graph(data,"price","$","Energy Cost","green")
-plt.show()
 
-""""
+plot_graph(data, "load_profile", "kW", "Load profile", "#B55E60")
+plot_graph(data, "battery_levels", "kWh", "Battery Level", "#609E6E")
+plot_graph(data, "energy_grid", "kW", "Power from/to Grid", "#5975A5")
+plot_graph(data, "energy_pv", "kW", "Power from PV", "#CC8962")
+plot_graph(data,"price","$","Energy Cost","green")
+
 print("L'utente ha speso/guadagnato avendo i pannelli solari e la batteria:")
 print(profit(data))
 #disabilito sia la batteria che il pannello, costi se non avessi i pannellis
@@ -24,6 +24,6 @@ energy_request(data)
 print("L'utente ha speso/guadagnato non avendo i pannelli solari e la batteria:")
 print(profit(data))
 
-"""
+plt.show()
 
 
